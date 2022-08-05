c3b3ccd277a9db6d5642d6cfc1fa652bea83e5b3
pome-ta <53405097+pome-ta@users.noreply.github.com>
2022-08-05 23:46:21 +0900

    add theme

--- docs/js/app.bundle.js
+++ docs/js/app.bundle.js
@@ -23655,7 +23655,180 @@ const autoCloseTags = /*@__PURE__*/EditorView.inputHandler.of((view, from, to, t
     return true;
 });
 
+// Using https://github.com/one-dark/vscode-one-dark-theme/ as reference for the colors
+const chalky = "#e5c07b", coral = "#e06c75", cyan = "#56b6c2", invalid = "#ffffff", ivory = "#abb2bf", stone = "#7d8799", // Brightened compared to original to increase contrast
+malibu = "#61afef", sage = "#98c379", whiskey = "#d19a66", violet = "#c678dd", darkBackground = "#21252b", highlightBackground = "#2c313a", background = "#282c34", tooltipBackground = "#353a42", selection = "#3E4451", cursor = "#528bff";
+/**
+The editor theme styles for One Dark.
+*/
+const oneDarkTheme = /*@__PURE__*/EditorView.theme({
+    "&": {
+        color: ivory,
+        backgroundColor: background
+    },
+    ".cm-content": {
+        caretColor: cursor
+    },
+    ".cm-cursor, .cm-dropCursor": { borderLeftColor: cursor },
+    "&.cm-focused .cm-selectionBackground, .cm-selectionBackground, .cm-content ::selection": { backgroundColor: selection },
+    ".cm-panels": { backgroundColor: darkBackground, color: ivory },
+    ".cm-panels.cm-panels-top": { borderBottom: "2px solid black" },
+    ".cm-panels.cm-panels-bottom": { borderTop: "2px solid black" },
+    ".cm-searchMatch": {
+        backgroundColor: "#72a1ff59",
+        outline: "1px solid #457dff"
+    },
+    ".cm-searchMatch.cm-searchMatch-selected": {
+        backgroundColor: "#6199ff2f"
+    },
+    ".cm-activeLine": { backgroundColor: highlightBackground },
+    ".cm-selectionMatch": { backgroundColor: "#aafe661a" },
+    "&.cm-focused .cm-matchingBracket, &.cm-focused .cm-nonmatchingBracket": {
+        backgroundColor: "#bad0f847",
+        outline: "1px solid #515a6b"
+    },
+    ".cm-gutters": {
+        backgroundColor: background,
+        color: stone,
+        border: "none"
+    },
+    ".cm-activeLineGutter": {
+        backgroundColor: highlightBackground
+    },
+    ".cm-foldPlaceholder": {
+        backgroundColor: "transparent",
+        border: "none",
+        color: "#ddd"
+    },
+    ".cm-tooltip": {
+        border: "none",
+        backgroundColor: tooltipBackground
+    },
+    ".cm-tooltip .cm-tooltip-arrow:before": {
+        borderTopColor: "transparent",
+        borderBottomColor: "transparent"
+    },
+    ".cm-tooltip .cm-tooltip-arrow:after": {
+        borderTopColor: tooltipBackground,
+        borderBottomColor: tooltipBackground
+    },
+    ".cm-tooltip-autocomplete": {
+        "& > ul > li[aria-selected]": {
+            backgroundColor: highlightBackground,
+            color: ivory
+        }
+    }
+}, { dark: true });
+/**
+The highlighting style for code in the One Dark theme.
+*/
+const oneDarkHighlightStyle = /*@__PURE__*/HighlightStyle.define([
+    { tag: tags.keyword,
+        color: violet },
+    { tag: [tags.name, tags.deleted, tags.character, tags.propertyName, tags.macroName],
+        color: coral },
+    { tag: [/*@__PURE__*/tags.function(tags.variableName), tags.labelName],
+        color: malibu },
+    { tag: [tags.color, /*@__PURE__*/tags.constant(tags.name), /*@__PURE__*/tags.standard(tags.name)],
+        color: whiskey },
+    { tag: [/*@__PURE__*/tags.definition(tags.name), tags.separator],
+        color: ivory },
+    { tag: [tags.typeName, tags.className, tags.number, tags.changed, tags.annotation, tags.modifier, tags.self, tags.namespace],
+        color: chalky },
+    { tag: [tags.operator, tags.operatorKeyword, tags.url, tags.escape, tags.regexp, tags.link, /*@__PURE__*/tags.special(tags.string)],
+        color: cyan },
+    { tag: [tags.meta, tags.comment],
+        color: stone },
+    { tag: tags.strong,
+        fontWeight: "bold" },
+    { tag: tags.emphasis,
+        fontStyle: "italic" },
+    { tag: tags.strikethrough,
+        textDecoration: "line-through" },
+    { tag: tags.link,
+        color: stone,
+        textDecoration: "underline" },
+    { tag: tags.heading,
+        fontWeight: "bold",
+        color: coral },
+    { tag: [tags.atom, tags.bool, /*@__PURE__*/tags.special(tags.variableName)],
+        color: whiskey },
+    { tag: [tags.processingInstruction, tags.string, tags.inserted],
+        color: sage },
+    { tag: tags.invalid,
+        color: invalid },
+]);
+/**
+Extension to enable the One Dark theme (both the editor theme and
+the highlight style).
+*/
+const oneDark = [oneDarkTheme, /*@__PURE__*/syntaxHighlighting(oneDarkHighlightStyle)];
+
+const codeSample = `function initShader() {
+  gl = cxtCanvas.getContext('webgl2');
+  //gl = cxtCanvas.getContext('webgl');
+  const prg = create_program(
+    create_shader('vs', vertexPrimitive),
+    create_shader('fs', fragmentPrimitive)
+  );
+  uniLocation[0] = gl.getUniformLocation(prg, 'time');
+  uniLocation[1] = gl.getUniformLocation(prg, 'mouse');
+  uniLocation[2] = gl.getUniformLocation(prg, 'resolution');
+
+  const position = new Float32Array([
+    -1.0, 1.0, 0.0, 1.0, 1.0, 0.0, -1.0, -1.0, 0.0, 1.0, -1.0, 0.0,
+  ]);
+  const index = new Uint16Array([0, 2, 1, 1, 2, 3]);
+
+  const vPosition = create_vbo(position);
+  const vIndex = create_ibo(index);
+  const vAttLocation = gl.getAttribLocation(prg, 'vertexPosition');
+
+  const VERTEX_SIZE = 3; // vec3
+
+  gl.bindBuffer(gl.ARRAY_BUFFER, vPosition);
+  gl.enableVertexAttribArray(vAttLocation);
+  gl.vertexAttribPointer(vAttLocation, VERTEX_SIZE, gl.FLOAT, false, 0, 0);
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, vIndex);
+
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);
+}
+
+// シェーダを生成する関数
+function create_shader(type, text) {
+  let shader;
+  // scriptタグのtype属性をチェック
+  switch (type) {
+    // 頂点シェーダの場合
+    case 'vs':
+      shader = gl.createShader(gl.VERTEX_SHADER);
+      break;
+    // フラグメントシェーダの場合
+    case 'fs':
+      shader = gl.createShader(gl.FRAGMENT_SHADER);
+      break;
+    default:
+      return;
+  }
+
+  // 生成されたシェーダにソースを割り当てる
+  gl.shaderSource(shader, text);
+  // シェーダをコンパイルする
+  gl.compileShader(shader);
+  // シェーダが正しくコンパイルされたかチェック
+  if (gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+    // 成功していたらシェーダを返して終了
+    return shader;
+  } else {
+    // 失敗していたらエラーログをアラートしコンソールに出力
+    // alert(gl.getShaderInfoLog(shader));
+    console.log(gl.getShaderInfoLog(shader));
+  }
+}
+`;
+
 new EditorView({
-  extensions: [basicSetup, javascript()],
+  doc: codeSample,
+  extensions: [basicSetup, EditorView.lineWrapping, javascript(), oneDark],
   parent: document.body,
 });
--- docs/js/app.js
+++ docs/js/app.js
@@ -1,7 +1,72 @@
 import { EditorView, basicSetup } from 'codemirror';
 import { javascript } from '@codemirror/lang-javascript';
+import { oneDark } from '@codemirror/theme-one-dark';
+
+const codeSample = `function initShader() {
+  gl = cxtCanvas.getContext('webgl2');
+  //gl = cxtCanvas.getContext('webgl');
+  const prg = create_program(
+    create_shader('vs', vertexPrimitive),
+    create_shader('fs', fragmentPrimitive)
+  );
+  uniLocation[0] = gl.getUniformLocation(prg, 'time');
+  uniLocation[1] = gl.getUniformLocation(prg, 'mouse');
+  uniLocation[2] = gl.getUniformLocation(prg, 'resolution');
+
+  const position = new Float32Array([
+    -1.0, 1.0, 0.0, 1.0, 1.0, 0.0, -1.0, -1.0, 0.0, 1.0, -1.0, 0.0,
+  ]);
+  const index = new Uint16Array([0, 2, 1, 1, 2, 3]);
+
+  const vPosition = create_vbo(position);
+  const vIndex = create_ibo(index);
+  const vAttLocation = gl.getAttribLocation(prg, 'vertexPosition');
+
+  const VERTEX_SIZE = 3; // vec3
+
+  gl.bindBuffer(gl.ARRAY_BUFFER, vPosition);
+  gl.enableVertexAttribArray(vAttLocation);
+  gl.vertexAttribPointer(vAttLocation, VERTEX_SIZE, gl.FLOAT, false, 0, 0);
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, vIndex);
+
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);
+}
+
+// シェーダを生成する関数
+function create_shader(type, text) {
+  let shader;
+  // scriptタグのtype属性をチェック
+  switch (type) {
+    // 頂点シェーダの場合
+    case 'vs':
+      shader = gl.createShader(gl.VERTEX_SHADER);
+      break;
+    // フラグメントシェーダの場合
+    case 'fs':
+      shader = gl.createShader(gl.FRAGMENT_SHADER);
+      break;
+    default:
+      return;
+  }
+
+  // 生成されたシェーダにソースを割り当てる
+  gl.shaderSource(shader, text);
+  // シェーダをコンパイルする
+  gl.compileShader(shader);
+  // シェーダが正しくコンパイルされたかチェック
+  if (gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+    // 成功していたらシェーダを返して終了
+    return shader;
+  } else {
+    // 失敗していたらエラーログをアラートしコンソールに出力
+    // alert(gl.getShaderInfoLog(shader));
+    console.log(gl.getShaderInfoLog(shader));
+  }
+}
+`;
 
 let view = new EditorView({
-  extensions: [basicSetup, javascript()],
+  doc: codeSample,
+  extensions: [basicSetup, EditorView.lineWrapping, javascript(), oneDark],
   parent: document.body,
 });
