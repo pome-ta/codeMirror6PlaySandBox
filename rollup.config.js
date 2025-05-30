120d834018a3151403104eeb1e94d624249897a9
pome-ta <53405097+pome-ta@users.noreply.github.com>
2025-05-30 15:54:26 +0900

    name revate docs

--- rollup.config.js
+++ rollup.config.js
@@ -3,9 +3,9 @@ import { nodeResolve } from '@rollup/plugin-node-resolve';
 
 const codemirror = () => {
   return {
-    input: './client/js/editor/codemirror/bundleSrc/codemirror.src.js',
+    input: './docs/js/editor/codemirror/bundleSrc/codemirror.src.js',
     output: {
-      file: './client/js/editor/codemirror/codemirror.bundle.js',
+      file: './docs/js/editor/codemirror/codemirror.bundle.js',
       format: 'es',
     },
     plugins: [nodeResolve()],
