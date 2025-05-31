e8258e0d582158d8b4d4d23d4d2661fd0ec80b38
pome-ta <53405097+pome-ta@users.noreply.github.com>
2025-05-31 12:40:14 +0900

    up

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,107 +1,28 @@
 import Editor from './editor/index.js';
 
+const codePath = './js/editor/index.js';
+
+
+/* -- load Source */
+async function fetchSketchFile(path) {
+  const res = await fetch(path);
+  const sketchText = await res.text();
+  return sketchText;
+}
+
+
+/*
 const editorDiv = document.createElement('div');
 editorDiv.id = 'editor-div';
 editorDiv.style.width = '100%';
 //editorDiv.style.backgroundColor = 'dodgerblue'
 //editorDiv.style.backgroundColor = 'darkslategray';
 
-const docs = `import { minimalSetup } from './codemirror/codemirror.js';
-import { EditorState, } from './codemirror/state.js';
-import { EditorView, lineNumbers, highlightActiveLineGutter } from './codemirror/view.js';
-import { closeBrackets, autocompletion } from './codemirror/autocomplete.js';
-
-/*
-const basicSetup = (() => [
-    lineNumbers(),
-    highlightActiveLineGutter(),
-    highlightSpecialChars(),
-    history(),
-    foldGutter(),
-    drawSelection(),
-    dropCursor(),
-    EditorState.allowMultipleSelections.of(true),
-    indentOnInput(),
-    syntaxHighlighting(defaultHighlightStyle, { fallback: true }),
-    bracketMatching(),
-    closeBrackets(),
-    autocompletion(),
-    rectangularSelection(),
-    crosshairCursor(),
-    highlightActiveLine(),
-    highlightSelectionMatches(),
-    keymap.of([
-        ...closeBracketsKeymap,
-        ...defaultKeymap,
-        ...searchKeymap,
-        ...historyKeymap,
-        ...foldKeymap,
-        ...completionKeymap,
-        ...lintKeymap
-    ])
-])();
-*/
-/*
-const minimalSetup = (() => [
-    highlightSpecialChars(),
-    history(),
-    drawSelection(),
-    syntaxHighlighting(defaultHighlightStyle, { fallback: true }),
-    keymap.of([
-        ...defaultKeymap,
-        ...historyKeymap,
-    ])
-])();
-
-*/
-
-const initTheme = EditorView.theme(
-  {
-    '&': {
-      fontSize: '0.72rem',
-      //fontSize: '1rem',
-    },
-    '.cm-scroller': {
-      fontFamily:
-        'Consolas, Menlo, Monaco, source-code-pro, Courier New, monospace',
-    },
-    '.cm-line': { padding: 0 },
-    '&.cm-editor': {
-      '&.cm-focused': {
-        outline: '0px dotted #21212100',
-      },
-    },
-  },
-  { dark: true },
-);
-
-const initializeSetup = [
-  minimalSetup,
-  lineNumbers(),
-  highlightActiveLineGutter(),
-  closeBrackets(),
-  initTheme,
-];
-
-
-class Editor {
-  constructor(editorDiv, doc = '') {
-    this.state = EditorState.create({
-      doc: doc,
-      extensions: initializeSetup,
-    });
-    this.editor = new EditorView({
-      state: this.state,
-      parent: editorDiv,
-    })
-  }
-}
-
-export default Editor;
-`;
+const docs = `import { minimalSetup } from './codemirror/codemirror.js';`;
 
 const editor = new Editor(editorDiv, docs);
 
 document.addEventListener('DOMContentLoaded', () => {
   document.body.appendChild(editorDiv);
 });
+*/
