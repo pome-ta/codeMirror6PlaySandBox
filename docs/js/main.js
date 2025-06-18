9d7627988703415658f2bd210e4a102099d1a357
pome-ta <53405097+pome-ta@users.noreply.github.com>
2025-06-18 18:28:52 +0900

    not

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,5 +1,163 @@
 import Editor from './editor/index.js';
 
+
+class Elementer {
+  // header footer をいい感じに管理したい(Elementor じゃなくてもいいか、、)
+
+  #element;
+
+  constructor(type, idName = null, classNames = []) {
+    this.#element = document.createElement(type);
+    if (idName !== null) {
+      this.#element.id = idName;
+    }
+    classNames.forEach((name) => {
+      this.#element.classList.add(name);
+    });
+    this.addStyles();
+  }
+
+  get element() {
+    return this.#element;
+  }
+
+  static of(type, idName, classNames) {
+    const instance = new this(type, idName, classNames);
+    return instance.element;
+  }
+
+  addStyles() {
+    this.#element.style.position = 'sticky';
+    this.#element.style.display = 'flex';
+    this.#element.style.alignItems = 'center';
+    this.#element.style.width = '100%';
+  }
+}
+
+
+const createHeader = (idName = null, classNames = []) => {
+  const element = Elementer.of('header', idName, classNames);
+  element.style.top = '0';
+  element.style.backgroundColor = 'maroon';
+  element.style.zIndex = '1';
+
+  return element;
+};
+
+
+const createFooter = (idName = null, classNames = []) => {
+  const element = Elementer.of('footer', idName, classNames);
+  element.style.padding = '0.6rem 0';
+  element.style.justifyContent = 'space-around';
+  element.style.backgroundColor = 'maroon';
+  element.style.bottom = '0';
+
+  return element;
+};
+
+
+
+class AccessoryWidgets {
+  constructor(targetEditor, isMobile) {
+    this.targetEditor = targetEditor
+    this.isMobile = isMobile;
+    this.header = createHeader('header');
+    if (this.isMobile) {
+      this.footer = createFooter('footer');
+      //this.footer.style.display = 'none';
+    }
+  }
+  
+  #setupItems = (items, parent) => items.forEach((item) => parent?.appendChild(item));
+  
+  setupHeader(items) {
+    //this.#setupItems(items, this.header);
+    items.forEach((item) => this.header.appendChild(item));
+  }
+  
+  setupFooter(items) {
+    if (!this.isMobile) {
+      return
+    }
+    //this.#setupItems(items, this.footer);
+    items.forEach((item) => this.footer.appendChild(item));
+  }
+  
+  
+  visualViewportHandler() {
+    this.header.style.top = `${window.visualViewport.offsetTop}px`;
+    const upBottom = window.innerHeight
+        - window.visualViewport.height
+        + window.visualViewport.offsetTop
+        - window.visualViewport.pageTop;
+    
+    this.footer.style.bottom = `${upBottom}px`;
+  }
+  
+  /*
+  eventtHandler() {
+    const visualViewportHandler = () => {
+      console.log(this.targetEditor.hasFocus)
+      this.header.style.top = `${window.visualViewport.offsetTop}px`;
+      
+      //this.footer.style.display = targetEditor.hasFocus ? 'flex' : 'none';
+      const upBottom = window.innerHeight
+        - window.visualViewport.height
+        + window.visualViewport.offsetTop
+        - window.visualViewport.pageTop;
+      
+      console.log(`accessory: ${upBottom}`)
+      console.log(`accessory: ${this.footer}`)
+      
+      this.footer.style.bottom = `${upBottom}px`;
+      //this.footer.style.button = `${0}px`;
+    }
+    
+    window.visualViewport.addEventListener('resize', visualViewportHandler);
+    window.visualViewport.addEventListener('scroll', visualViewportHandler);
+  }
+  */
+
+}
+
+
+const btnW = '2.5rem';
+const btnRadius = '16%';
+
+function _createButtonWrap(width, height) {
+  const wrap = document.createElement('div');
+  // xxx: 最大数問題
+  wrap.style.minWidth = width;
+  wrap.style.height = height;
+  wrap.style.display = 'flex';
+  wrap.style.justifyContent = 'center';
+  wrap.style.alignItems = 'center';
+  return wrap;
+}
+
+function createIcon(char) {
+  const icon = document.createElement('span');
+  icon.textContent = char;
+  icon.style.fontSize = '1.0rem';
+  icon.style.color = '#f2f2f7'; // gray6
+  return icon;
+}
+
+function createActionButton(iconChar) {
+  const wrap = _createButtonWrap(btnW, '100%');
+  const button = _createButtonWrap('90%', '90%');
+  const icon = createIcon(iconChar);
+  wrap.appendChild(button);
+  wrap.style.cursor = 'pointer';
+  button.style.borderRadius = btnRadius;
+  button.style.backgroundColor = '#8e8e93'; // light gray
+  button.style.filter = 'drop-shadow(2px 2px 2px rgba(28, 28, 30, 0.9))';
+  button.appendChild(icon);
+  return wrap;
+}
+
+
+
 /* --- load Source */
 async function insertFetchDoc(filePath) {
   const fetchFilePath = async (path) => {
@@ -40,85 +198,52 @@ const editor = Editor.create(editorDiv);
 
 
 /* --- accessory */
+const h1Tag = document.createElement('h1');
+h1Tag.style.fontSize = '1.5rem';
+h1Tag.textContent = 'Safari Virtual Keyboard Demo';
 
-const accessoryStyle = `
-  position: sticky;
-  display: flex;
-  align-items: center;
-  width = 100%;
-`;
+const [
+  commentButton,
+  selectLineButton,
+  leftButton,
+  downButton,
+  upButton,
+  rightButton,
+  selectAllButton,
+  redoButton,
+  undoButton,
+  //reIndentButton,
+] = ['//', '▭', '←', '↓', '↑', '→', '⎁', '⤻', '⤺'].map((str) => {
+  const ele = createActionButton(str);
+  //footerDiv.appendChild(ele);
+  return ele;
+});
 
-function createHeader() {
-  const h1Tag = document.createElement('h1');
-  h1Tag.style.fontSize = '1.5rem';
-  h1Tag.textContent = 'Safari Virtual Keyboard Demo';
-  
-  const element = document.createElement('header');
-  element.id = 'header';
-  element.style.cssText = accessoryStyle;
-  element.style.top = '0';
-  element.style.backgroundColor = 'maroon';
-  element.style.zIndex = 1;
-  
-  element.appendChild(h1Tag);
-  
-  return element;
-}
-
-
-function createFooter() {
-  const h1Tag = document.createElement('h1');
-  h1Tag.style.fontSize = '1.5rem';
-  h1Tag.textContent = 'Safari Virtual Keyboard Demo';
-  
-  const element = document.createElement('footer');
-  element.id = 'footer';
-  element.style.cssText = accessoryStyle;
-  element.style.bottom = '0';
-  element.style.backgroundColor = 'navy';
-  //element.style.zIndex = 1;
-  //element.style.cssText = accessoryStyle;
-  
-  element.appendChild(h1Tag);
-  
-  return element;
-}
-
-/*
-element.style.position = 'sticky';
-    element.style.display = 'flex';
-    element.style.alignItems = 'center';
-    //element.style.justifyContent = 'stretch';
-    element.style.width = '100%';
-*/
-const accessoryHeader = createHeader();
-const accessoryFooter = createFooter();
+const buttons = [commentButton,
+  selectLineButton,
+  leftButton,
+  downButton,
+  upButton,
+  rightButton,
+  selectAllButton,
+  redoButton,
+  undoButton,];
 
 
-const visualViewportHandler = () => {
-console.log('---');
-console.log(`window.innerHeight:${window.innerHeight}`);
-console.log(`window.visualViewport.height:${window.visualViewport.height}`);
-console.log(`window.visualViewport.height:${window.visualViewport.offsetTop}`);
-    
-  const offsetTop = visualViewport.offsetTop;
-  const offsetBottom = window.innerHeight - window.visualViewport.height + offsetTop - window.visualViewport.pageTop;
-  
-  accessoryHeader.style.top = `${offsetTop}px`;
-  accessoryFooter.style.bottom = `${offsetBottom}px`;
-  //accessoryFooter.style.bottom = `${window.visualViewport.height}px`;
-}
+const accessory = new AccessoryWidgets(editor,true);
+accessory.setupHeader([h1Tag]);
+accessory.setupFooter(buttons);
 
-
-window.visualViewport.addEventListener('resize', visualViewportHandler);
-window.visualViewport.addEventListener('scroll', visualViewportHandler);
 
 
 document.addEventListener('DOMContentLoaded', () => {
-  rootDiv.appendChild(accessoryHeader);
+  rootDiv.appendChild(accessory.header);
   rootDiv.appendChild(editorDiv);
-  rootDiv.appendChild(accessoryFooter);
+  rootDiv.appendChild(accessory.footer);
   document.body.appendChild(rootDiv);
+  
+  
+  
 
   insertFetchDoc(codeFilePath).then((loadedSource) => {
     // todo: 事前に`doc` が存在するなら、`doc` 以降にテキストを挿入
@@ -128,6 +253,7 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 });
 
-// window.addEventListener('load', () => {
-//   console.log(editor.hasFocus);
-// });
+window.addEventListener('load', () => {
+   window.visualViewport.addEventListener('resize', accessory.visualViewportHandler());
+  window.visualViewport.addEventListener('scroll', accessory.visualViewportHandler());
+});
