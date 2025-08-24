870e5d5a4c4d8d7f40f1c2ddfe1adcdd87ddd19b
pome-ta <53405097+pome-ta@users.noreply.github.com>
2025-08-24 13:28:47 +0900

    simple

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,1 +1,8 @@
-console.log('h');
+import { basicSetup, EditorView } from 'codemirror';
+
+const view = new EditorView({
+  doc: 'hoge',
+  extensions: [basicSetup],
+  parent: document.body,
+});
+console.log(view);
