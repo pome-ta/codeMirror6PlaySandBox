8fe1849cded683edeeddba728f05d24b201cea45
pome-ta <53405097+pome-ta@users.noreply.github.com>
2025-09-21 17:15:23 +0900

    up

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,18 +1,44 @@
 import { EditorView, lineNumbers } from '@codemirror/view';
 import {Compartment, EditorState, StateEffect, StateField,} from '@codemirror/state';
+import {javascript} from "@codemirror/lang-javascript";
+import { basicSetup} from "codemirror";
+
+
+const docs = `import { EditorView, lineNumbers } from '@codemirror/view';
+import {Compartment, EditorState, StateEffect, StateField,} from '@codemirror/state';
+import {javascript} from "@codemirror/lang-javascript";
+import { basicSetup} from "codemirror";
 
 
 const extensions = [
-  lineNumbers,
+  basicSetup,
+  lineNumbers(),
 ];
 
 const state =EditorState.create({
     doc: 'hoge1loO0',
-    //extensions: extensions,
+    extensions: extensions,
   });
 
 const view = new EditorView({
   state: state,
   parent: document.body,
 });
-console.log(view);
+console.log(basicSetup);`
+
+
+const extensions = [
+  basicSetup,
+  lineNumbers(),
+];
+
+const state =EditorState.create({
+    doc: docs,
+    extensions: extensions,
+  });
+
+const view = new EditorView({
+  state: state,
+  parent: document.body,
+});
+console.log(basicSetup);
