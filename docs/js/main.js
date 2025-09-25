edbec77e703fe32b47b731187e83df48dc0a53c5
pome-ta <53405097+pome-ta@users.noreply.github.com>
2025-09-25 15:57:23 +0900

    lsp?

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,17 +1,15 @@
 import {EditorState,} from '@codemirror/state';
 import {EditorView,} from '@codemirror/view';
 
-import {LSPClient, languageServerExtensions} from '@codemirror/lsp-client';
+import {languageServerExtensions, LSPClient} from '@codemirror/lsp-client';
 import {typescriptLanguage} from '@codemirror/lang-javascript';
-
-
 
 
 //import {minimalSetup} from 'codemirror';
 import {basicSetup} from 'codemirror';
 
 
-const dummyCodePath = './dummyCode.js'
+const dummyCodePath = './dummyCode.js';
 
 const getSource = async (path) => {
   const res = await fetch(path);
@@ -19,29 +17,34 @@ const getSource = async (path) => {
   return text;
 };
 
-/*
+
 function simpleWebSocketTransport(uri) {
   let handlers = [];
   let sock = new WebSocket(uri);
-  sock.onmessage = e => { for (let h of handlers) h(e.data.toString()) }
+  sock.onmessage = e => {
+    for (let h of handlers) h(e.data.toString());
+  };
   return new Promise(resolve => {
     sock.onopen = () => resolve({
-      send(message) { sock.send(message) },
-      subscribe((value) => void) { handlers.push(handler) },
-      unsubscribe(handler: (value: string) => void) { handlers = handlers.filter(h => h != handler) }
-    })
-  })
+      send: (message) => {
+        sock.send(message);
+      },
+      subscribe: (handler) => {
+        handlers.push(handler);
+      },
+      unsubscribe: (handler) => {
+        handlers = handlers.filter(h => h != handler);
+      }
+    });
+  });
 }
-*/
-
-let transport = await simpleWebSocketTransport("ws://host:port")
-let client = new LSPClient({extensions: languageServerExtensions()}).connect(transport)
 
 
+let transport = await simpleWebSocketTransport('ws://host:port');
+let client = new LSPClient({extensions: languageServerExtensions()}).connect(transport);
 
 
-
-const createEditor = (parent=null) => {
+const createEditor = (parent = null) => {
   const customTheme = EditorView.theme(
     {
       '&': {
@@ -53,26 +56,25 @@ const createEditor = (parent=null) => {
       dark: false,
     },
   );
-  
+
   const extensions = [
     basicSetup,
     //minimalSetup,
     customTheme,
     typescriptLanguage,
   ];
-  
+
   const state = EditorState.create({
     extensions: extensions,
   });
-  
+
   const view = new EditorView({
     state: state,
     parent: parent ? parent : document.body,
   });
-  
+
   return view;
 };
-
 
 
 const editor = createEditor();
